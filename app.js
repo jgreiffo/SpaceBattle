1398f7c4d44daa34d0636d72ce4906aadfe5d8b1
jgreiffo <jgreiffo@gmail.com>
2025-01-05 21:08:34 -0500

    Add files via upload
    style

--- app.js
+++ app.js
@@ -1,153 +1,128 @@
-class AlienShip extends SpaceShip {
-    constructor(index) {
-      const hull = Math.floor(Math.random() * 4) + 3; 
-      const firepower = Math.floor(Math.random() * 3) + 2; 
-      const accuracy = Math.random() * 0.2 + 0.6; 
-      super(`Alien Ship ${index}`, hull, firepower, accuracy);
-    }
+class Game {
+	constructor() {
+	  this.humanShip = this.createHumanShip("USS Assembly");
+	  this.alienFleet = this.createAlienFleet(6); // Create a fleet of 6 alien ships with unique stats
+	}
   
-    attackTarget(target) {
-      console.log(`${this.name} is attacking ${target.name}!`);
+	// Method to create the human ship
+	createHumanShip(name) {
+	  return {
+		name: name,
+		hull: 20,
+		firepower: 5,
+		accuracy: 0.7,
+		attackTarget: (target) => {
+		  console.log(`${this.humanShip.name} is attacking ${target.name}!`);
   
-      // Exactitud
-      if (Math.random() < this.accuracy) {
-        console.log(`Direct hit! ${target.name} takes ${this.firepower} damage.`);
-        target.hull -= this.firepower;
+		  if (Math.random() < this.humanShip.accuracy) {
+			console.log(`Direct hit! ${target.name} takes ${this.humanShip.firepower} damage.`);
+			target.hull -= this.humanShip.firepower;
   
-        // Check if the target is destroyed
-        if (target.hull <= 0) {
-          console.log(`${target.name} has been destroyed!`);
-        } else {
-          console.log(`${target.name}'s remaining hull: ${target.hull}`);
-        }
-      } else {
-        console.log(`${this.name} missed the attack!`);
-      }
-    }
+			if (target.hull <= 0) {
+			  console.log(`${target.name} has been destroyed!`);
+			} else {
+			  console.log(`${target.name}'s remaining hull: ${target.hull}`);
+			}
+		  } else {
+			console.log(`${this.humanShip.name} missed the attack!`);
+		  }
+		},
+	  };
+	}
+  
+	// Method to create alien ships
+	createAlienFleet(count) {
+	  const fleet = [];
+	  for (let i = 1; i <= count; i++) {
+		const alienShip = {
+		  name: `Alien Ship ${i}`,
+		  hull: Math.floor(Math.random() * 4) + 3, // Hull between 3 and 6
+		  firepower: Math.floor(Math.random() * 3) + 2, // Firepower between 2 and 4
+		  accuracy: Math.random() * 0.2 + 0.6, // Accuracy between 0.6 and 0.8
+		  attackTarget: (target) => {
+			console.log(`${this.name} is attacking ${target.name}!`);
+  
+			if (Math.random() < this.accuracy) {
+			  console.log(`Direct hit! ${target.name} takes ${this.firepower} damage.`);
+			  target.hull -= this.firepower;
+  
+			  if (target.hull <= 0) {
+				console.log(`${target.name} has been destroyed!`);
+			  } else {
+				console.log(`${target.name}'s remaining hull: ${target.hull}`);
+			  }
+			} else {
+			  console.log(`${this.name} missed the attack!`);
+			}
+		  },
+		};
+		fleet.push(alienShip);
+	  }
+	  return fleet;
+	}
+  
+	// Method to check if there is a winner
+	checkWin() {
+	  if (this.humanShip.hull <= 0) {
+		console.log(`${this.humanShip.name} went destroyed! The aliens have won!`);
+		return true;
+	  }
+	  const remainingAliens = this.alienFleet.filter(alien => alien.hull > 0);
+	  if (remainingAliens.length === 0) {
+		console.log(`All alien ships went annihilaaa-ted! ${this.humanShip.name} has won!`);
+		return true;
+	  }
+	  return false;
+	}
+  
+	// Method to start the battle
+	start() {
+	  console.log("Welcome to the Space Battle Game!");
+	  console.log("\nYour ship:", this.humanShip);
+	  console.log("Alien Fleet:");
+	  this.alienFleet.forEach(alienShip => console.log(alienShip));
+  
+	  console.log("\nThe battle begins!");
+  
+	  // Loop all alien ships in the fleet
+	  for (let i = 0; i < this.alienFleet.length; i++) {
+		const currentAlien = this.alienFleet[i];
+		console.log(`\nFighting ${currentAlien.name}...`);
+		this.battle(currentAlien);
+  
+		if (this.checkWin()) {
+		  break; // Stop the game if either the human ship or all aliens are defeated
+		}
+	  }
+	}
+  
+	// Method -> battle between the human ship and an alien ship
+	battle(alienShip) {
+	  // Repeat until the human ship or the alien ships hull reaches 0 
+	  while (this.humanShip.hull > 0 && alienShip.hull > 0) {
+		// Human ship attacks
+		this.humanShip.attackTarget(alienShip);
+		if (alienShip.hull <= 0) break;
+  
+		// Alien ship contra-ataca
+		alienShip.attackTarget(this.humanShip);
+		if (this.humanShip.hull <= 0) break;
+  
+		// Console log at the end of the round
+		console.log("\n-- End of Round Status --");
+		console.log(`${this.humanShip.name} - Hull: ${this.humanShip.hull}`);
+		console.log(`${alienShip.name} - Hull: ${alienShip.hull}`);
+	  }
+	}
   }
-  
-  class SpaceShip {
-    constructor(name, hull, firepower, accuracy) {
-      this.name = name;
-      this.hull = hull;
-      this.firepower = firepower;
-      this.accuracy = accuracy;
-    }
-  
-    attackTarget(target) {
-    }
-  }
-  
-  class Game {
-    constructor() {
-      this.humanShip = new HumanShip("USS Assembly");
-      this.alienFleet = this.createAlienFleet(5); // 
-      this.currentAlienIndex = 0; 
-	  // ^ Start with the first alien ship ^^
-    }
-  
-    createAlienFleet(count) {
-      const fleet = [];
-      for (let i = 0; i < count; i++) {
-        fleet.push(new AlienShip(i + 1)); // Create each alien ship with unique stats
-      }
-      return fleet;
-    }
-  
-    checkWin() {
-      if (this.humanShip.hull <= 0) {
-        console.log(`${this.humanShip.name} went destroyed! The aliens have won!`);
-        return true;
-      }
-      const remainingAliens = this.alienFleet.filter(alien => alien.hull > 0);
-      if (remainingAliens.length === 0) {
-        console.log(`All alien ships went annihilaaa-ted! ${this.humanShip.name} has won!`);
-        return true;
-      }
-      return false;
-    }
-  
-    start() {
-      console.log("Welcome to the Space Battle Game!");
-      console.log("\nYour ship:", this.humanShip);
-      console.log("Alien Fleet:");
-      this.alienFleet.forEach(alienShip => console.log(alienShip));
-  
-      console.log("\nThe battle begins!");
-      while (this.humanShip.hull > 0 && this.currentAlienIndex < this.alienFleet.length) {
-        const currentAlien = this.alienFleet[this.currentAlienIndex];
-        console.log(`\n${this.humanShip.name} is facing ${currentAlien.name}`);
-  
-        this.battle(currentAlien);
-  
-        if (this.checkWin()) return;
-  
-        if (currentAlien.hull <= 0) {
-          console.log(`${currentAlien.name} has been destroyed!`);
-          this.currentAlienIndex++;
-        }
-  
-        if (this.currentAlienIndex < this.alienFleet.length) {
-          const retreat = prompt("Do you want to retreat? (yes/no)").toLowerCase();
-          if (retreat === "yes") {
-            console.log(`${this.humanShip.name} retreats. Game over.`);
-            return;
-          }
-        }
-      }
-  
-      if (this.checkWin()) return;
-    }
-  
-    battle(alienShip) {
-      // Repeat until either the human ship or the alien ship's hull reaches 0 or below
-      while (this.humanShip.hull > 0 && alienShip.hull > 0) {
-        // Human ship attacks
-        this.humanShip.attackTarget(alienShip);
-        if (alienShip.hull <= 0) break;
-  
-        // Alien ship counterattacks
-        alienShip.attackTarget(this.humanShip);
-        if (this.humanShip.hull <= 0) break;
-  
-        // Status console log at the end of the round
-        console.log("\n-- End of Round Status --");
-        console.log(`${this.humanShip.name} - Hull: ${this.humanShip.hull}`);
-        console.log(`${alienShip.name} - Hull: ${alienShip.hull}`);
-      }
-    }
-  }
-  
-  class HumanShip extends SpaceShip {
-    constructor(name) {
-      super(name, 20, 5, 0.7); // Fixed stats for the human ship
-    }
-  
-    attackTarget(target) {
-      console.log(`${this.name} is attacking ${target.name}!`);
-  
-      // Check accuracy
-      if (Math.random() < this.accuracy) {
-        console.log(`Direct hit! ${target.name} takes ${this.firepower} damage.`);
-        target.hull -= this.firepower;
-  
-        // Check if the target is destroyed
-        if (target.hull <= 0) {
-          console.log(`${target.name} has been destroyed!`);
-        } else {
-          console.log(`${target.name}'s remaining hull: ${target.hull}`);
-        }
-      } else {
-        console.log(`${this.name} missed the attack!`);
-      }
-    }
-  }
+
   
   // Instantiate and start the game
   const spaceBattleGame = new Game();
   spaceBattleGame.start();
   
-   //   // Instantiate USS Assembly and a single Alien Ship
+
+  //   // Instantiate USS Assembly and a single Alien Ship
 // const humanShip = new HumanShip("USS Assembly");
 // const alienShip = new AlienShip(1);
 
